a6aedb542a07cec63981035306b19837875c6458
Tusharkale0912 <Tushar.kale@phntechnology.com>
2025-05-22 17:59:51 +0530

    second largest element

--- geeksforgeeks160/secondlargest.py
+++ geeksforgeeks160/secondlargest.py
@@ -6,7 +6,6 @@ Input: arr[] = [12, 35, 1, 10, 34, 1]
 Output: 34
 Explanation: The largest element of the array is 35 and the second largest element is 34
 
-
 Input: arr[] = [10, 5, 10]
 Output: 5
 Explanation: The largest element of the array is 10 and the second largest element is 5.
