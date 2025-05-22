9d19859bb33cf8c4a91c8a21b6509a9f5943d18f
Tusharkale0912 <Tushar.kale@phntechnology.com>
2025-05-22 17:57:24 +0530

    Factorial Recursion

--- Questions/30Questions/two.py
+++ Questions/30Questions/two.py
@@ -4,7 +4,15 @@
 
 '''
 
-def findfactorialNum():
-    pass
+num = int(input("Enter a num:"))
+def factorialNo(num):
+    if num<0:
+        return 
+    elif num==0 or  num==1:
+        return 1
+    else:
+        return num*factorialNo(num-1)
+
+print(factorialNo(num))
 
 
