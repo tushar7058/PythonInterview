1d9d693e1a17231f3f889b8733c3ed5b512ff422
Tusharkale0912 <Tushar.kale@phntechnology.com>
2025-05-23 00:09:08 +0530

    use of hashmap in python

---
+++ Questions/30Questions/useofhashmaopInPython.py
@@ -0,0 +1,16 @@
+
+def first_unique_char(s):
+    freq_map = {}  # character: count
+
+    # Step 1: Count frequency of each character
+    for char in s:
+        freq_map[char] = freq_map.get(char, 0) + 1
+
+    # Step 2: Find the first character with count = 1
+    for index, char in enumerate(s):
+        if freq_map[char] == 1:
+            return index
+
+    return -1
+
+
